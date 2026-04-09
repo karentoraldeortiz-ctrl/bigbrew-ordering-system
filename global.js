f39167f6f4580ff04d7b6571ff1dc6d3f06407f3
Karen Ortiz <karentoraldeortiz@gmail.com>
2026-04-09 15:35:47 +0800

    3:35pm push

---
+++ global.js
@@ -0,0 +1,11 @@
+const hamburger = document.getElementById("hamburger");
+      const navLinks = document.getElementById("navlinks");
+
+      hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+      });
+
+        // function toggleMenu() { 
+        //     const navLinks = document.getElementById("navlinks");
+        //     navLinks.classList.toggle("active");
+        // }       
