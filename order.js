5a61d840e1b0735e869c8250af96d91b60b57ad2
Karen Ortiz <karentoraldeortiz@gmail.com>
2026-04-10 23:17:34 +0800

    day 100

--- order.js
+++ order.js
@@ -6,3 +6,40 @@ document.querySelectorAll('.flip-btn').forEach(button => {
        card.classList.toggle('flip');
 });
 });
+
+
+const filterButtons = document.querySelectorAll('.tab'); 
+const menuItems = document.querySelectorAll('.card'); 
+const noResultsMessage = document.getElementById('no-results'); 
+
+filterButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const selectedCategory = button.getAttribute('data-category');
+        let hasVisibleItems = false;
+
+        menuItems.forEach(item => {
+          
+            if (selectedCategory === 'all' || selectedCategory === 'milk-tea') {
+                
+              
+                if (selectedCategory === 'all' || item.classList.contains('milk-tea')) {
+                    item.style.display = 'block';
+                    hasVisibleItems = true;
+                } else {
+                    item.style.display = 'none';
+                }
+
+            } else {
+               
+                item.style.display = 'none';
+            }
+        });
+
+      
+        if (!hasVisibleItems) {
+            noResultsMessage.style.display = 'block';
+        } else {
+            noResultsMessage.style.display = 'none';
+        }
+    });
+});
