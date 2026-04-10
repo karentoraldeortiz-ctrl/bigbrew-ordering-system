a9c2b7bc6311d484b53436076ca4be5dba4fc0fb
Karen Ortiz <karentoraldeortiz@gmail.com>
2026-04-10 10:09:58 +0800

    bounce na ko maem

---
+++ main.js
@@ -0,0 +1,9 @@
+
+// steps.forEach(step => {
+//     const btn = step.querySelector(".toggle-btn");
+
+//     btn.addEventListener("click", () => {
+//         steps.forEach(s => s.classList.remove("active"));
+//         step.classList.toggle("active");
+//     });
+// });
