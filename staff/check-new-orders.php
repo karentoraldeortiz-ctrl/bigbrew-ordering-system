a85fd9f7f8d648dd4fa7d21cf34a6d5447094d5e
Allyana Flores <allyanaflores014@gmail.com>
2026-05-09 19:29:59 +0800

    7PM-5/9 | Staff Side slighly polished

--- staff/check-new-orders.php
+++ staff/check-new-orders.php
@@ -5,12 +5,10 @@ if (!isset($_SESSION['staff_logged_in'])) {
     exit;
 }
 include "../db.php";
- 
-$since = isset($_GET['since']) ? mysqli_real_escape_string($conn, $_GET['since']) : date('Y-m-d H:i:s');
- 
-$q = mysqli_query($conn, "SELECT COUNT(*) as cnt FROM orders WHERE created_at > '$since' AND order_status = 'pending'");
+
+$q = mysqli_query($conn, "SELECT COUNT(*) as cnt FROM orders WHERE order_status = 'pending'");
 $row = mysqli_fetch_assoc($q);
- 
+
 header('Content-Type: application/json');
 echo json_encode(['new_orders' => (int)$row['cnt']]);
 ?>
